c6481a31ec318ca91a5400fd1b4ee08ff3e94b26
Frix-x <boisselier.felix@gmail.com>
2015-06-13 22:28:30 +0200

    Organize imports

--- app/src/main/java/fr/utc/frixx/payutc_android/MainActivity.java
+++ app/src/main/java/fr/utc/frixx/payutc_android/MainActivity.java
@@ -3,7 +3,6 @@ package fr.utc.frixx.payutc_android;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
